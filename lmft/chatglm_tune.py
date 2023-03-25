1f48103c9c21d9398148c941ab15bcc7fa2d08c8
shibing624 <shibing624@126.com>
2023-03-25 20:52:52 +0800

    update chatglm finetune.

---
+++ lmft/chatglm_tune.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: 
+"""
+from transformers import TrainingArguments, Trainer, HfArgumentParser
+from transformers import AutoTokenizer, AutoModel
+from transformers.trainer import TRAINING_ARGS_NAME
+import torch
+import torch.nn as nn
+from peft import get_peft_model, LoraConfig, TaskType
+from dataclasses import dataclass, field
+import datasets
+from typing import Optional
+import os
+
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune, or train from scratch.
+    """
+
+    model_name_or_path: Optional[str] = field(default="THUDM/chatglm-6b")
+
+
+@dataclass
+class DataTrainingArguments:
+    dataset_name: Optional[str] = field(
+        default="shibing624/alpaca-zh", metadata={"help": "The name of the dataset to use (via the datasets library)."}
+    )
+    model_path: str = field(default="output")
+    lora_name: str = field(default="chatglm6b-lora.pt")
+    lora_rank: int = field(default=8)
+
+
+class CastOutputToFloat(nn.Sequential):
+    def forward(self, x):
+        return super().forward(x).to(torch.float32)
+
+
+def get_masks_and_position_ids(
+        seq, seq_len, context_length, device, gmask=False, position_encoding_2d=True
+):
+    mask_position = (
+            seq_len - 2
+    )  # is equal to `seq.index(mask_token)` or `seq.index(150001)`
+    attention_mask = torch.ones((1, context_length, context_length), device=device)
+    attention_mask.tril_()
+    attention_mask[..., : mask_position - 1] = 1
+    attention_mask = (attention_mask < 0.5).bool()
+
+    if position_encoding_2d:
+        seq_length = seq_len - 1  # is equal to `seq_length = seq.index(150004)`
+        position_ids = torch.arange(context_length, dtype=torch.long, device=device)
+        if not gmask:
+            position_ids[seq_length:] = mask_position
+        block_position_ids = torch.cat(
+            (
+                torch.zeros(seq_length, dtype=torch.long, device=device),
+                torch.arange(
+                    context_length - seq_length, dtype=torch.long, device=device
+                )
+                + 1,
+            )
+        )
+        position_ids = torch.stack((position_ids, block_position_ids), dim=0)
+    else:
+        position_ids = torch.arange(context_length, dtype=torch.long, device=device)
+        if not gmask:
+            position_ids[context_length - 1:] = mask_position
+    return attention_mask, position_ids
+
+
+def data_collator(batch, tokenizer) -> dict:
+    len_ids = [len(feature["input_ids"]) for feature in batch]
+    longest = max(len_ids)
+    input_ids = []
+    attention_mask_list = []
+    position_ids_list = []
+    labels_list = []
+    for ids_l, feature in sorted(zip(len_ids, batch), key=lambda x: -x[0]):
+        ids = feature["input_ids"]
+        seq_len = feature["seq_len"]
+        labels = (
+                [-100] * (seq_len - 1)
+                + ids[(seq_len - 1):]
+                + [-100] * (longest - ids_l)
+        )
+        ids = ids + [tokenizer.pad_token_id] * (longest - ids_l)
+        _ids = torch.LongTensor(ids)
+        attention_mask, position_ids = get_masks_and_position_ids(
+            ids, seq_len, longest, _ids.device, gmask=False
+        )
+        labels_list.append(torch.LongTensor(labels))
+        input_ids.append(_ids)
+        attention_mask_list.append(attention_mask)
+        position_ids_list.append(position_ids)
+    input_ids = torch.stack(input_ids)
+    labels = torch.stack(labels_list)
+    attention_mask = torch.stack(attention_mask_list)
+    position_ids = torch.stack(position_ids_list)
+    return {
+        "input_ids": input_ids,
+        "labels": labels,
+        "attention_mask": attention_mask,
+        "position_ids": position_ids,
+    }
+
+
+class ModifiedTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        return model(
+            input_ids=inputs["input_ids"],
+            attention_mask=inputs["attention_mask"],
+            position_ids=inputs["position_ids"],
+            labels=inputs["labels"],
+        ).loss
+
+    def save_model(self, output_dir=None, _internal_call=False):
+        os.makedirs(output_dir, exist_ok=True)
+        torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
+        save_tunable_parameters(self.model, os.path.join(output_dir, self.args.lora_name))
+
+
+def save_tunable_parameters(model, path):
+    saved_params = {
+        k: v.to("cpu") for k, v in model.named_parameters() if v.requires_grad
+    }
+    torch.save(saved_params, path)
+
+
+def main():
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+    model_args, training_args = parser.parse_args_into_dataclasses()
+
+    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, trust_remote_code=True)
+    model = AutoModel.from_pretrained(
+        model_args.model_name_or_path, trust_remote_code=True, device_map="auto"
+    ).half().cuda()
+    model.gradient_checkpointing_enable()
+    model.enable_input_require_grads()
+    model.is_parallelizable = True
+    model.model_parallel = True
+    model.lm_head = CastOutputToFloat(model.lm_head)
+    model.config.use_cache = (False)
+
+    # setup peft
+    peft_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM,
+        inference_mode=False,
+        r=training_args.lora_rank,
+        lora_alpha=32,
+        lora_dropout=0.1,
+    )
+    model = get_peft_model(model, peft_config)
+
+    # load dataset
+    ds = datasets.load_dataset(training_args.dataset_name)
+
+    # start train
+    trainer = ModifiedTrainer(
+        model=model,
+        train_dataset=ds,
+        args=training_args,
+        data_collator=data_collator,
+        tokenizer=tokenizer,
+    )
+    trainer.train()
+
+    # save model
+    save_tunable_parameters(
+        model, os.path.join(training_args.output_dir, training_args.lora_name)
+    )
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -10,21 +10,35 @@ exec(open('lmft/version.py').read())
 if sys.version_info < (3,):
     sys.exit('Sorry, Python3 is required.')
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    readme = f.read()
+extras = {}
+extras["dev"] = ["torch"]
+extras["test"] = extras["dev"] + ["pytest", "pytest-xdist"]
 
 setup(
     name='lmft',
     version=__version__,
-    description='Language Model Fine-tuning Toolkit',
-    long_description=readme,
-    long_description_content_type='text/markdown',
+    description='Language Model Fine-tuning Toolkit (LMFT)',
+    license_files=["LICENSE"],
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     author='XuMing',
     author_email='xuming624@qq.com',
     url='https://github.com/shibing624/lmft',
     license="Apache License 2.0",
     zip_safe=False,
-    python_requires=">=3.6.0",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={},
+    python_requires=">=3.7.0",
+    keywords='lmft,GPT2,transformers,pytorch,language model',
+    install_requires=[
+        "loguru",
+        "peft",
+        "transformers",
+        "datasets",
+        "tqdm",
+    ],
+    extras_require=extras,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -35,15 +49,22 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
-    keywords='LLM,lmft,GPT2,transformers,pytorch,language model',
-    install_requires=[
-        "loguru",
-        "peft",
-        "transformers",
-        "datasets",
-        "tqdm",
-    ],
-    packages=find_packages(exclude=['tests']),
-    package_dir={'lmft': 'lmft'},
-    package_data={'lmft': ['*.*', 'data/*.txt']}
 )
+
+# Release checklist
+# 1. Change the version in __init__.py and setup.py.
+# 2. Commit these changes with the message: "Release: VERSION"
+# 3. Add a tag in git to mark the release: "git tag VERSION -m 'Adds tag VERSION for pypi' "
+#    Push the tag to git: git push --tags origin main
+# 4. Run the following commands in the top-level directory:
+#      python setup.py bdist_wheel
+#      python setup.py sdist
+# 5. Upload the package to the pypi test server first:
+#      twine upload dist/* -r pypitest
+#      twine upload dist/* -r pypitest --repository-url=https://test.pypi.org/legacy/
+# 6. Check that you can install it in a virtualenv by running:
+#      pip install -i https://testpypi.python.org/pypi peft
+# 7. Upload the final version to actual pypi:
+#      twine upload dist/* -r pypi
+# 8. Add release notes to the tag in github once everything is looking hunky-dory.
+# 9. Update the version in __init__.py, setup.py to the new version "-dev" and push to master
