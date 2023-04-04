058f5d592f62177984dcff0116be6ab1295a78c6
shibing624 <shibing624@126.com>
2023-04-04 10:58:03 +0800

    update hf dataset.

--- examples/training_chatglm_hfdataset_demo.py
+++ examples/training_chatglm_hfdataset_demo.py
@@ -14,16 +14,17 @@ from lmft import ChatGLMTune
 def finetune_demo():
     parser = argparse.ArgumentParser()
     parser.add_argument('--train_file', default='tatsu-lab/alpaca', type=str,
-                        help='Datasets name, eg: tatsu-lab/alpaca or shibing624/alpaca-zh')
+                        help='Dataset name (e.g. tatsu-lab/alpaca, shibing624/alpaca-zh, BelleGroup/train_1M_CN, '
+                             'Chinese-Vicuna/guanaco_belle_merge_v1.0)')
     parser.add_argument('--model_type', default='chatglm', type=str, help='Transformers model type')
     parser.add_argument('--model_name', default='THUDM/chatglm-6b', type=str, help='Transformers model or path')
     parser.add_argument('--do_train', action='store_true', help='Whether to run training.')
     parser.add_argument('--do_predict', action='store_true', help='Whether to run predict.')
-    parser.add_argument('--output_dir', default='./outputs-alpaca/', type=str, help='Model output directory')
+    parser.add_argument('--output_dir', default='./outputs/alpaca/', type=str, help='Model output directory')
     parser.add_argument('--max_seq_length', default=256, type=int, help='Input max sequence length')
     parser.add_argument('--max_length', default=256, type=int, help='Output max sequence length')
     parser.add_argument('--num_epochs', default=1, type=int, help='Number of training epochs')
-    parser.add_argument('--batch_size', default=2, type=int, help='Batch size')
+    parser.add_argument('--batch_size', default=3, type=int, help='Batch size')
     args = parser.parse_args()
     logger.info(args)
     model = None
@@ -52,6 +53,13 @@ def finetune_demo():
                 args={'use_lora': True, 'eval_batch_size': args.batch_size,
                       'output_dir': args.output_dir, "max_length": args.max_length, }
             )
+        sents = [
+            '问：用一句话描述地球为什么是独一无二的。\n答：',
+            '问：给定两个数字，计算它们的平均值。 数字: 25, 36\n答：',
+            '问：基于以下提示填写以下句子的空格。 空格应填写一个形容词 句子: ______出去享受户外活动，包括在公园里散步，穿过树林或在海岸边散步。\n答：',
+        ]
+        response = model.predict(sents)
+        print(response)
         response, history = model.chat("给出三个保持健康的秘诀。", history=[])
         print(response)
         response, history = model.chat("describe atom's struct。", history=history)
@@ -60,6 +68,8 @@ def finetune_demo():
 
         ref_model = ChatGLMTune(args.model_type, args.model_name,
                                 args={'use_lora': False, 'eval_batch_size': args.batch_size})
+        response = ref_model.predict(sents)
+        print(response)
         response, history = ref_model.chat("给出三个保持健康的秘诀。", history=[])
         print(response)
         response, history = ref_model.chat("describe atom's struct。", history=history)
